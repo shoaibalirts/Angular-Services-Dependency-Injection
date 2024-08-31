e39940739e28b1d22aea028175a3c6edc4a6d3aa
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-31 13:22:26 +0200

    added updateTaskStatus service and used it in tasks-list and execute it in task-item where it is required

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable({
   providedIn: 'root',
@@ -7,6 +7,7 @@ import { Task } from './task.model';
 export class TasksService {
   private tasks = signal<Task[]>([]);
   allTasks = this.tasks.asReadonly(); // we want to protect it to change the data from outside (component)
+  // service # 1
   addTask(taskData: { title: string; description: string }) {
     const newTask: Task = {
       ...taskData,
@@ -15,4 +16,14 @@ export class TasksService {
     };
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
   }
-}
+  // service # 2
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update(
+      (oldTasks) =>
+        oldTasks.map((task) =>
+          task.id === taskId ? { ...task, status: newStatus } : task
+        )
+      // if id found then ...task is the old task to be updated to a new status, else no change
+    );
+  }
+} // end of service class
