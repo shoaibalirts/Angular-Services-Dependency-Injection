a8bce5b09ab01fd0975211d74228032ed3a98fdf
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-31 11:22:02 +0200

    injected tasks service into new-task component

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -11,7 +12,20 @@ import { FormsModule } from '@angular/forms';
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  // private tasksService: TasksService;
+  // shorter way of requesting a service:
+  // 1. remove above line:   private tasksService: TasksService;
+  // 2. write private in front of tService and replace tService to tasksService
+  constructor(private tasksService: TasksService) {
+    // constructor(tService: TasksService) {
+    // tService is an instance of the TaskService provided by angular
+    // this.tasksService = tService;
+  }
+
   onAddTask(title: string, description: string) {
+    // this.tasksService.addTask({ title: title, description: description });
+    this.tasksService.addTask({ title, description }); // shorthand by JS, tsksService is from constructor
+
     this.formEl()?.nativeElement.reset();
   }
 }
